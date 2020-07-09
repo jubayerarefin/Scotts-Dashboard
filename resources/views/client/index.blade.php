be94dbd338725a2a16e1dfc01464ea3fcb7fd967
Shane Sweeney <shanep.sween@gmail.com>
2020-07-08 19:51:39 -0500

    clean-up

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -16,10 +16,75 @@
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.js"></script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
 </head>
 
 
 <body>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                    @endif
+                    @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!--  -->
+    <!--  -->
+    <!--  -->
+    <!-- Start of Content -->
+    <!--  -->
+    <!--  -->
+    <!--  -->
+    <!--  -->
     <div class="container">
         <div class="row ">
             <div class="col-md-4 input-daterange">
@@ -36,51 +101,59 @@
         </div>
         <div class="row">
             <div class="table-responsive">
-                <table class="table table-striped table-hover table-bordered display nowrap data-table"
-                    style="width:100%" id="table_content">
-                    <thead>
-                        <tr>
-                            <th data-class-name="priority">
-                                User ID
-                            </th>
-                            <th>
-                                Email
-                            </th>
-                            <th>
-                                First Name
-                            </th>
-                            <th>
-                                Last Name
-                            </th>
-                            <th>
-                                Organization Name
-                            </th>
-                            <th>
-                                Scott's Org ID
-                            </th>
-                            <th style="white-space: inherit">
-                                Last Journal Update
-                                (Time)
-                            </th>
-                            <th style="white-space: inherit">
-                                Last Journal Update
-                                (Date)
-                            </th>
-                            <th style="white-space: inherit">
-                                Created At (User)
-                            </th>
-                            <th style="white-space: inherit">
-                                Created At
-                                (Organization)
-                            </th>
-                            <th>
-                                Subscribed until
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
+                <form method="POST" id="convert_form" action="export.php">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <input type="hidden" name="file_content" id="file_content" />
+                            <button type="button" name="convert" id="convert" class="btn btn-light">Export</button>
+                        </div>
+                    </div>
+                    <table class="table table-striped table-hover table-bordered display nowrap data-table"
+                        style="width:100%" id="table_content">
+                        <thead>
+                            <tr>
+                                <th data-class-name="priority">
+                                    User ID
+                                </th>
+                                <th>
+                                    Email
+                                </th>
+                                <th>
+                                    First Name
+                                </th>
+                                <th>
+                                    Last Name
+                                </th>
+                                <th>
+                                    Organization Name
+                                </th>
+                                <th>
+                                    Scott's Org ID
+                                </th>
+                                <th style="white-space: inherit">
+                                    Last Journal Update
+                                    (Time)
+                                </th>
+                                <th style="white-space: inherit">
+                                    Last Journal Update
+                                    (Date)
+                                </th>
+                                <th style="white-space: inherit">
+                                    Created At (User)
+                                </th>
+                                <th style="white-space: inherit">
+                                    Created At
+                                    (Organization)
+                                </th>
+                                <th>
+                                    Subscribed until
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </form>
             </div>
         </div>
     </div>
@@ -96,6 +169,14 @@ $(document).ready(function() {
         autoclose: true
     });
 
+    $('#convert').click(function() {
+        var table_content = '<table>';
+        table_content += $('#table_content').html();
+        table_content += '</table>';
+        $('#file_content').val(table_content);
+        $('#convert_form').submit();
+    });
+
     load_data();
 
     function load_data(from_date = null, to_date = null) {
@@ -108,7 +189,25 @@ $(document).ready(function() {
             ],
             dom: 'Bfrtip',
             buttons: [
-                'excel', 'csv', 'pdf', 'copy'
+                'copy', 'print',
+                {
+                    extend: 'excel',
+                    filename: 'PartDetails',
+                    footer: true
+                },
+                {
+                    extend: 'pdf',
+                    filename: 'PartDetails'
+                },
+                {
+                    extend: 'csvHtml5',
+                    filename: 'PartDetails'
+                },
+                {
+                    extend: 'collection',
+                    text: 'columns',
+                    buttons: ['columnsVisibility']
+                }
             ],
             stateSave: true,
             ajax: {
--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,3 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/clients', ['uses' => 'ClientController@index', 'as' => 'clients.index']);
-
-Route::resource('daterange', 'DateRangeController');
